50eff861527eec3ce6f527e5c4913d24c2236d36
adityadoang <101446409+adityadoang@users.noreply.github.com>
2025-05-04 11:02:36 +0700

    tambah file index.php untuk menampilkan nama

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $name= "Aditya";
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>halaman awal</title>
+    </head>
+    <body>
+        <h1>Halo nama saya <?php echo $gitname ?></h1>
+    </body>
+    </html>
